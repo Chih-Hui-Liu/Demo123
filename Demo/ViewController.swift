c79912ea590d45c8e957ed519c88add33b42bd29
Leo 柳 <edcqazwsx321@gmail.com>
2020-11-19 14:17:51 +0800

    g

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -6,12 +6,23 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let hand = UIImage(named: "hand")
+        let handView = UIImageView(image: hand)
+        
+       let url = URL(string: "https://bit.ly/3kLlPRp")
+              let player = AVPlayer(url: url!)
+              let playerLayer = AVPlayerLayer(player: player)
+              playerLayer.videoGravity = .resizeAspectFill
+              view.layer.addSublayer(playerLayer)
+              playerLayer.frame = CGRect(origin: CGPoint(x: 30, y: 50), size: handView.frame.size)
+              playerLayer.mask = handView.layer
+              player.play()
+ 
     }
 
 
